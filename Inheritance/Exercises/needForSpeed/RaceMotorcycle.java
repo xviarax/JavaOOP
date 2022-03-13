da788d378d9440aece20971aa98e73f027a4a484
Viara Marinova <100493985+xviarax@users.noreply.github.com>
2022-03-13 14:37:51 +0200

    Add files via upload

---
+++ Inheritance/Exercises/needForSpeed/RaceMotorcycle.java
@@ -0,0 +1,9 @@
+package needForSpeed;
+
+public class RaceMotorcycle extends Motorcycle{
+    private final static double DEFAULT_FUEL_CONSUMPTION = 8;
+    public RaceMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        this.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
