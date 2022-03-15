863045547e99efb4b301d30755c54a618a6dc18d
Viara Marinova <100493985+xviarax@users.noreply.github.com>
2022-03-15 11:11:59 +0200

    Add files via upload

---
+++ Interfaces and abstraction/Exercises/BirthdayCelebrations/Citizen.java
@@ -0,0 +1,30 @@
+package BirthdayCelebrations;
+
+
+
+public class Citizen implements  Birthable, Identifiable {
+    private String name;
+    private int age;
+    private String id;
+    private String birthDate;
+
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+
+}
---
+++ Interfaces and abstraction/Exercises/DefineanInterfacePerson/Person.java
@@ -0,0 +1,6 @@
+package DefineanInterfacePerson;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
